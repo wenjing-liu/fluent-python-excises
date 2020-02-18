daa9606167f40cc390ae7077666fb6bd503298a2
Claire Liu <wenjing.liu09@foxmail.com>
2020-02-18 13:58:53 +0800

    add chapter 08 examples

---
+++ chapter_08/8_12.py
@@ -0,0 +1,30 @@
+class HauntedBus:
+  def __init__(self, passengers=[]):
+    self.passengers = passengers
+  
+  def pick(self, name):
+    self.passengers.append(name)
+  
+  def drop(self, name):
+    self.passengers.remove(name)
+
+bus1 = HauntedBus(['Alice', 'Bill'])
+print(bus1.passengers)
+bus1.pick('Charlie')
+bus1.drop('Alice')
+print(bus1.passengers)
+
+print('===================================================')
+bus2 = HauntedBus()
+bus2.pick('Carrie')
+print(bus2.passengers)
+bus3 = HauntedBus()
+print(bus3.passengers)
+bus3.pick('Dave')
+print(bus2.passengers)
+print(bus2.passengers is bus3.passengers)
+print(bus1.passengers)
+
+print('===================================================')
+print(dir(HauntedBus.__init__))
+print(HauntedBus.__init__.__defaults__)
