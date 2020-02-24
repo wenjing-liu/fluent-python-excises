f59e9ba6fdfaa77b00d3b0e2d66ab4b6a0f69ab6
Claire Liu <wenjing.liu09@foxmail.com>
2020-02-24 14:02:52 +0800

    add chapter 18 examples

---
+++ chapter_18/18_05.py
@@ -0,0 +1,85 @@
+import asyncio
+import os
+import time
+import sys
+from concurrent import futures
+import requests
+
+import aiohttp
+
+POP20_CC = ('CN IN US ID BR PK NG BD RU JP MX PH VN ET EG DE IR CD FR').split()
+BASE_URL = 'http://flupy.org/data/flags'
+DEST_DIR = './'
+
+MAX_WORKERS = 20
+
+def save_flag(img, filename):
+  path = os.path.join(DEST_DIR, filename)
+  with open(path, 'wb') as fp:
+    fp.write(img)
+
+def get_flag(cc):
+  url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
+  resp = requests.get(url)
+  return resp.content
+
+def show(text):
+  print(text, end=' ')
+  sys.stdout.flush()
+
+def download_one(cc):
+  image = get_flag(cc)
+  show(cc)
+  save_flag(image, cc.lower() + '.gif')
+  return cc
+
+def download_many(cc_list):
+  workers = min(MAX_WORKERS, len(cc_list))
+  with futures.ThreadPoolExecutor(workers) as executor:
+    to_do = []
+    for cc in sorted(cc_list):
+      future = executor.submit(download_one, cc)
+      to_do.append(future)
+      msg = 'Scheduled for {}: {}'
+      print(msg.format(cc, future))
+    
+    results = []
+    for future in futures.as_completed(to_do):
+      res = future.result()
+      msg = '{} result: {!r}'
+      print(msg.format(future, res))
+      results.append(res)
+
+  return len(list(results))
+
+def main(download_many):
+  t0 = time.time()
+  count = download_many(POP20_CC)
+  elapsed = time.time() - t0
+  msg = '\n{} flags downloaded in {:.2f}s'
+  print(msg.format(count, elapsed))
+
+@asyncio.coroutine
+def get_flag(cc):
+  url = '{}/{cc}/{cc}.gif'.format(BASE_URL, cc=cc.lower())
+  resp = yield from aiohttp.request('GET', url)
+  image = yield from resp.read()
+  return image
+
+@asyncio.coroutine
+def download_one(cc):
+  image = yield from get_flag(cc)
+  show(cc)
+  save_flag(image, cc.lower + '.gif')
+  return cc
+
+def download_many(cc_list):
+  loop = asyncio.get_event_loop()
+  to_do = [download_one(cc) for cc in sorted(cc_list)]
+  wait_coro = asyncio.wait(to_do)
+  res, _ = loop.run_until_complete(wait_coro)
+  loop.close()
+  return len(res)
+
+if __name__ = '__main__':
+  main(download_many)
